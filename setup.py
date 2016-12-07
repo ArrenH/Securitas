fcce3634533e34ea06d5a530db675e98bf725734
ArrenHgit config --global user.name ArrenHgit config --global user.name ArrenH <huynhall@msu.edu>
2016-12-07 00:40:23 -0500

    added setup files to config for pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'Securitas_python_package',
+    packages = ['symantec_package'],
+    version = 'v1.0',  # Ideally should be same as your github release tag varsion
+    description = 'Python SDK to increase productivity and ease implementation of Symantec Validation and ID Protection (VIP) which is a two factor authentication API',
+    author = 'Allen Huynh, Gabriel Morcote, Hanlin Ye',
+    author_email = '',
+    url = 'https://github.com/ArrenH/Securitas',
+    download_url = 'https://github.com/ArrenH/Securitas.git',
+    keywords = ['Symantec', 'VIP', 'Python', 'SOAP', 'Validation and Identity Protection', '2FA', 'two factor authentication'],
+    classifiers = [],
+)
