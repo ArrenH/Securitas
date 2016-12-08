5f7ab6a714a5da3b5a2363243eef830b19d592bc
Allen <huynhall@msu.edu>
2016-12-07 23:18:49 -0500

    Fixed pypi settings

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name = 'securitas',
-    packages = ['symantec_package'],
-    version = 'v1.0',  # Ideally should be same as your github release tag varsion
+    packages = find_packages(),
+    include_package_data=True,
+    version = 'v1.0.1',  # Ideally should be same as your github release tag varsion
     description = 'Python SDK to increase productivity and ease implementation of Symantec Validation and ID Protection (VIP) which is a two factor authentication API',
     author = 'Allen Huynh, Gabriel Morcote, Hanlin Ye',
     author_email = '',
+    install_requires= ['suds_jurko'],
     url = 'https://github.com/ArrenH/Securitas',
     download_url = 'https://github.com/ArrenH/Securitas/archive/v1.0.tar.gz',
     keywords = ['Symantec', 'VIP', 'Python', 'SOAP', 'Validation and Identity Protection', '2FA', 'two factor authentication'],
